35cb64d0d78d0fbc0193b69eeca286d06d7deca6
eneiceo <35782438+eneiceo@users.noreply.github.com>
2026-04-13 09:31:42 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'mywod-v2';
+const ASSETS = [
+  '/mywod/',
+  '/mywod/index.html',
+  '/mywod/manifest.json',
+  '/mywod/icons/icon-192.png',
+  '/mywod/icons/icon-512.png',
+  '/mywod/icons/icon-180.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request)
+        .then(res => {
+          if (e.request.method === 'GET' && res.status === 200) {
+            const clone = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, clone));
+          }
+          return res;
+        })
+        .catch(() => caches.match('/mywod/index.html'))
+      )
+  );
+});
